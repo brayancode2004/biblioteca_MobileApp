244494bb348699594ef05370de223894499dddd1
BrandonSAragon <codebrayan2004@gmail.com>
2023-12-31 21:39:13 -0600

    Primer Commit inicializando la app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>UamLibrary Mobile App</Text>
+      <Text>UamLibrary Mobile app</Text>
       <StatusBar style="auto" />
     </View>
   );
